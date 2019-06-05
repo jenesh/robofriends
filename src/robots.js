883126c1db9c174149e0342e62dd4f473faf814b
jenesh <jeneshnapit@Jeneshs-MBP.zyxel.com>
2019-06-05 19:51:50 -0400

    Created cards component, robot array for use, and rendered 3 card components using robots array.

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'William Yogi',
+        username: 'Yogi',
+        email: 'yog1yog1@laugh.tv'
+    },
+    {
+        id: 3,
+        name: 'Lucas Metal',
+        username: 'FluffyPancakes',
+        email: 'dessert5@hungry.now'
+    },
+    {
+        id: 4,
+        name: 'Broke Guitar',
+        username: 'OhMan',
+        email: 'yeeeeHawww@country.boy'
+    },
+    {
+        id: 5,
+        name: 'Skinner Phony',
+        username: 'Phon1bo1',
+        email: 'sk1nn3r@princi.pal'
+    },
+    {
+        id: 6,
+        name: 'Marge Das',
+        username: 'Vroom',
+        email: 'vroombr00m@fast.car'
+    },
+    {
+        id: 7,
+        name: 'Forrest Boink',
+        username: 'oinkOink',
+        email: '01nk01nk@pig.moo'
+    },
+    {
+        id: 8,
+        name: 'Sleepy Hollow',
+        username: 'Boo',
+        email: 'sc4ryyyy@behind.you'
+    },
+]
